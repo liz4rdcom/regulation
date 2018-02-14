5cd9286e5e2ecdb812cbf922b43a8ff478356557
irakli jiishkariani <irakli2692@gmail.com>
2018-02-14 19:46:35 +0400

    organization details some fields

--- public/src/components/organization/organization-details.vue
+++ public/src/components/organization/organization-details.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    details {{id}}
+    <div class="header">
+      <i class="fa fa-arrow-left fa-2x" @click="goBack"></i>
+      <!-- <b-button  variant="link">
+      </b-button> -->
+    </div>
+
+    <div class="org-info">
+      <b-card class="mb-2" header="ორგანიზაცია" header-bg-variant="secondary" header-text-variant="white">
+        <p><b>სტატუსი:</b> {{organization.status}}</p>
+        <p><b>Napr სტატუსი:</b> {{organization.naprStatus}}</p>
+        <p><b>სამმართველოს რეგისტრაციის N:</b> {{organization.managementRegistrationNumber}}</p>
+        <p><b>საიდენტიფიკაციო კოდი:</b> {{organization.taxCode}}</p>
+        <p><b>სახაზინო რეგისტრაციის N:</b> {{organization.treasuryRegistrationNumber}}</p>
+        <p><b>სახაზინო რეგისტრაციის თარიღი:</b> {{organization.treasuryRegistrationDate}}</p>
+        <p><b>სახაზინო რეგისტრაციის ადგილი:</b> {{organization.treasuryRegistrationPlace}}</p>
+        <p><b>ქართული დასახელება:</b> {{organization.georgianName}}</p>
+        <p><b>ლათინური დასახელება:</b> {{organization.latinName}}</p>
+      </b-card>
+
+    </div>
   </div>
 </template>
 
@@ -13,13 +32,30 @@ export default {
   data: () => ({
     organization: {}
   }),
-  created() {
-    let response = this.$http.get(baseUrl + '/' + this.id)
+  async created () {
+    let url = baseUrl + '/' + this.id
+    let response = await this.$http.get(url)
 
     this.organization = response.data
+  },
+  methods: {
+    goBack () {
+      this.$router.back()
+    }
   }
 }
 </script>
 
 <style scoped>
+.header {
+  /* background-color: rgb(231, 231, 231); */
+  text-align: left;
+  padding: 15px;
+  padding-left: 30px;
+}
+
+.org-info {
+  text-align: left;
+
+}
 </style>
