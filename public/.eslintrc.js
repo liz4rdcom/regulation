b4c94ec22f5c24ed3d8c8329e9dad2689fd3cd6e
irakli jiishkariani <irakli2692@gmail.com>
2018-02-21 18:20:43 +0400

    add rules to eslintrc public

--- public/.eslintrc.js
+++ public/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential', 
+    'plugin:vue/essential',
     // https://github.com/standard/standard/blob/master/docs/RULES-en.md
     'standard'
   ],
@@ -24,6 +24,26 @@ module.exports = {
     // allow async-await
     'generator-star-spacing': 'off',
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 1,
+    'no-unused-expressions': 1,
+    'arrow-body-style': 0,
+    'arrow-parens': 0,
+    'prefer-destructuring': 0,
+    'object-shorthand': 0,
+    'no-use-before-define': 2,
+    'class-methods-use-this': 0,
+    'no-param-reassign': 1,
+    'no-return-await': 0,
+    'no-plusplus': 1,
+    'space-before-function-paren': 0,
+    'semi': 1,
+    'no-unused-vars': 1,
+    'no-trailing-spaces': 1,
+    'no-multiple-empty-lines': 1,
+    'eol-last': 1,
+    'padded-blocks': 1,
+    'prefer-arrow-callback': 1,
+    'comma-dangle': 1
   }
 }
