7eb69074612b869c279b8d8cf3c5cce1ce0f01bb
irakli jiishkariani <irakli2692@gmail.com>
2018-03-01 16:01:30 +0400

    fix juridical and factual addresses set

--- public/src/components/organization/organization-add.vue
+++ public/src/components/organization/organization-add.vue
@@ -185,10 +185,18 @@ export default {
   },
   methods: {
     onJuridicalAddressChanged(location) {
-      this.organization.juridicalAddress = location
+      this.organization.juridicalAddress.region = location.locationName
+      this.organization.juridicalAddress.district = location.locationUnitName
+      this.organization.juridicalAddress.settlement = location.settlement
+      this.organization.juridicalAddress.addressDescription = location.address
+      this.organization.juridicalAddress.postalCode = location.postalCode
     },
     onFactualAddressChanged(location) {
-      this.organization.factualAddress = location
+      this.organization.factualAddress.region = location.locationName
+      this.organization.factualAddress.district = location.locationUnitName
+      this.organization.factualAddress.settlement = location.settlement
+      this.organization.factualAddress.addressDescription = location.address
+      this.organization.factualAddress.postalCode = location.postalCode
     },
     async onClinicalManagerAdd(manager) {
       manager.id = await this.newUniqueId()
