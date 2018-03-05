529fc8934fff81ed2444f4635cf79d21a2aa60b5
irakli jiishkariani <irakli2692@gmail.com>
2018-03-05 11:38:17 +0400

    notification component

---
+++ public/src/components/common/notify.vue
@@ -0,0 +1,60 @@
+<template>
+<div class="footer" v-if="dismissCountDown">
+  <h2></h2>
+  <b-alert :show="dismissCountDown" dismissible :variant="notificationType" @dismissed="dismissCountDown=0"
+    @dismiss-count-down="countDownChanged">
+    <p>{{notification}} - {{dismissCountDown}}</p>
+  </b-alert>
+
+</div>
+</template>
+
+<script>
+import {bus} from '../common/bus'
+
+const successDefaultText = 'ოპერაცია წარმატებით დასრულდა'
+
+export default {
+  name: 'notify',
+  data() {
+    return {
+      notification: successDefaultText,
+      dismissSecs: 10,
+      dismissCountDown: 0,
+      notificationType: 'success'
+    }
+  },
+  created() {
+    bus.$on('success', (text) => {
+      this.notification = text || successDefaultText
+      this.notificationType = 'success'
+      this.showAlert()
+    })
+    bus.$on('error', (error) => {
+      this.notification = error.response.data.message
+      this.notificationType = (error.response.status === 500) ? 'danger' :
+        'warning'
+      this.showAlert()
+    })
+  },
+  methods: {
+    countDownChanged(dismissCountDown) {
+      this.dismissCountDown = dismissCountDown
+    },
+    showAlert() {
+      this.dismissCountDown = this.dismissSecs
+    }
+  }
+}
+</script>
+
+<style scoped>
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  height: 70px;
+  width: 100%;
+  overflow: hidden;
+}
+</style>
