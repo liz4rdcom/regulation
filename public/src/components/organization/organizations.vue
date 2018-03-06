6803e21878c106af9c95570e30490c4834aa60d8
irakli jiishkariani <irakli2692@gmail.com>
2018-03-06 19:41:47 +0400

    advanced search some fields

--- public/src/components/organization/organizations.vue
+++ public/src/components/organization/organizations.vue
@@ -5,25 +5,104 @@
   </b-button>
   <br>
   <br>
-  <b-container>
-    <b-row>
-      <b-col cols="1"></b-col>
-      <b-col cols="8">
-        <b-form-input v-model="searchString" type="text" @keyup.enter.native="search">
-        </b-form-input>
-      </b-col>
-      <b-col cols="1">
-        <b-button variant="primary" @click="search">
-          <i class="fa fa-search"></i>
-        </b-button>
-      </b-col>
-    </b-row>
-  </b-container>
+  <div class="searchArea">
+    <b-form-checkbox v-model="showAdvancedSearch" class="mb-5 searchCheckbox">
+      გაფართოებული ძებნა
+    </b-form-checkbox>
+    <br>
+    <b-container v-if="!showAdvancedSearch">
+      <b-row>
+        <b-col>
+          <b-form-input v-model="searchString" type="text" @keyup.enter.native="search">
+          </b-form-input>
+        </b-col>
+        <b-col cols="1">
+          <b-button variant="primary" @click="search">
+            <i class="fa fa-search"></i>
+          </b-button>
+        </b-col>
+      </b-row>
+    </b-container>
+    <div v-if="showAdvancedSearch">
+      <b-form-group label="რეგულირების სახე">
+        <b-form-select v-model="searchParams.regulationType" @change="searchParams.businessType=null" class="col-md-5">
+          <template slot="first">
+            <option :value="null" selected>ყველა</option>
+          </template>
+          <option v-for="type in regulationTypes" :key="type.regulationType">{{type.regulationType}}</option>
+        </b-form-select>
+      </b-form-group>
+      <b-form-group label="საქმიანობის სახე">
+        <b-form-select v-model="searchParams.businessType" @change="searchParams.businessWithInvasiveAnesthesia = null" class="col-md-5">
+          <template slot="first">
+            <option :value="null" selected>ყველა</option>
+          </template>
+          <option v-for="type in getBusinessTypes(searchParams.regulationType)" :key="type">{{type}}</option>
+        </b-form-select>
+      </b-form-group>
+      <b-form-group label="საქმ. ინვაზ. გაუტკივარებით">
+        <b-form-select
+          v-model="searchParams.businessWithInvasiveAnesthesia"
+          :disabled="!hasBusinessesWithInvasiveAnesthesia(searchParams.regulationType, searchParams.businessType)"
+          class="col-md-5">
+          <template slot="first">
+            <option :value="null" selected>ყველა</option>
+          </template>
+          <option v-for="business in getBusinessesWithInvasiveAnesthesia(searchParams.regulationType, searchParams.businessType)" :key="business">{{business}}</option>
+        </b-form-select>
+      </b-form-group>
+      <b-form-group label="ბრძანების ტიპი">
+        <b-form-select v-model="searchParams.commandType" class="col-md-5">
+          <template slot="first">
+            <option :value="null" selected>ყველა</option>
+          </template>
+          <option v-for="type in commandTypes" :key="type">{{type}}</option>
+        </b-form-select>
+      </b-form-group>
+      <locations :locations="locations" searching @change="addressChanged"></locations>
+      <b-form-group label="სტატუსი">
+        <b-form-select v-model="searchParams.statusGeoName" class="col-md-5">
+          <template slot="first">
+            <option :value="null" selected>ყველა</option>
+          </template>
+          <option v-for="status in statuses" :key="status">{{status}}</option>
+        </b-form-select>
+      </b-form-group>
+      <b-form-group label="Napr სტატუსი">
+        <b-form-select v-model="searchParams.naprStatus" class="col-md-5">
+          <template slot="first">
+            <option :value="null" selected>ყველა</option>
+          </template>
+          <option v-for="status in naprStatuses" :key="status">{{status}}</option>
+        </b-form-select>
+      </b-form-group>
+      <b-form-group label="ორგანიზაციის ტიპი">
+        <b-form-select v-model="searchParams.organizationType" class="col-md-5">
+          <template slot="first">
+            <option :value="null" selected>ყველა</option>
+          </template>
+          <option v-for="type in organizationTypes" :key="type">{{type}}</option>
+        </b-form-select>
+      </b-form-group>
+      <b-form-group label="სამართლებრივი ფორმა">
+        <b-form-select v-model="searchParams.legalForm" class="col-md-5">
+          <template slot="first">
+            <option :value="null" selected>ყველა</option>
+          </template>
+          <option v-for="form in legalForms" :key="form">{{form}}</option>
+        </b-form-select>
+      </b-form-group>
+      <b-button variant="primary" @click="advancedSearch">
+        <i class="fa fa-search"></i>
+      </b-button>
+    </div>
+  </div>
+
   <br>
   <b-table bordered responsive :items="organizations" :fields="fields">
-    <span slot="region" slot-scope="data">{{data.item.factualAddress.region}}</span>
-    <span slot="district" slot-scope="data">{{data.item.factualAddress.district}}</span>
-    <span slot="addressDescription" slot-scope="data">{{data.item.factualAddress.addressDescription}}</span>
+    <span slot="region" slot-scope="data">{{data.item.factualAddress ? data.item.factualAddress.region: ''}}</span>
+    <span slot="district" slot-scope="data">{{data.item.factualAddress ? data.item.factualAddress.district: ''}}</span>
+    <span slot="addressDescription" slot-scope="data">{{data.item.factualAddress ? data.item.factualAddress.addressDescription: ''}}</span>
     <span slot="permissionNumber" slot-scope="data">{{getPermissionNumber(data.item)}}</span>
     <span slot="licenseNumbers" slot-scope="data">{{getLicensesText(data.item)}}</span>
     <span slot="messageNumbers" slot-scope="data">{{getMessagesText(data.item)}}</span>
@@ -43,6 +122,8 @@
 <script>
 import lib from '../../libs'
 import {baseUrl, permissionType, licenseType, messageType} from './organization-constants'
+import locationsCOmponent from '../common/locations'
+
 const listTextSeparator = ', '
 
 export default {
@@ -99,14 +180,42 @@ export default {
       }
     ],
     organizations: [],
-    searchString: ''
+    searchString: '',
+    showAdvancedSearch: false,
+    searchParams: {
+      statusGeoName: null,
+      naprStatus: null,
+      organizationType: null,
+      legalForm: null,
+      regulationType: null,
+      businessType: null,
+      businessWithInvasiveAnesthesia: null,
+      commandType: null
+    },
+    statuses: [],
+    naprStatuses: [],
+    organizationTypes: [],
+    legalForms: [],
+    commandTypes: [],
+    businessStatuses: [],
+    locations: [],
+    regulationTypes: []
   }),
   async created () {
-    let response = await this.$http.get(baseUrl)
+    let response
 
-    this.organizations = response.data
-
-    await Promise.all([
+    [
+      response,
+      this.statuses,
+      this.naprStatuses,
+      this.organizationTypes,
+      this.legalForms,
+      this.commandTypes,
+      this.businessStatuses,
+      this.locations,
+      this.regulationTypes
+    ] = await Promise.all([
+      this.$http.get(baseUrl),
       lib.fetchStatuses(),
       lib.fetchNaprStatuses(),
       lib.fetchOrganizationTypes(),
@@ -116,12 +225,21 @@ export default {
       lib.fetchLocations(),
       lib.fetchRegulationTypes()
     ])
+
+    this.organizations = response.data
   },
   methods: {
     async search() {
       let url = baseUrl + '/search'
 
       let response = await this.$http.get(url, {params: {query: this.searchString}})
+
+      this.organizations = response.data
+    },
+    async advancedSearch() {
+      let url = baseUrl + '/advancedSearch'
+
+      let response = await this.$http.get(url, {params: this.searchParams})
 
       this.organizations = response.data
     },
@@ -135,36 +253,86 @@ export default {
       this.$router.push('/edit/' + organization.id)
     },
     getPermissionNumber (organization) {
+      if (!organization.regulations) return null
+
       let permission = organization.regulations
         .filter(regulation => regulation.type === permissionType)[0]
 
       return permission ? permission.documentNumber : null
     },
     getLicensesText (organization) {
+      if (!organization.regulations) return null
+
       return organization.regulations
         .filter(regulation => regulation.type === licenseType)
         .map(license => license.documentNumber)
         .join(listTextSeparator)
     },
     getMessagesText (organization) {
+      if (!organization.regulations) return null
+
       return organization.regulations
         .filter(regulation => regulation.type === messageType)
         .map(message => message.documentNumber)
         .join(listTextSeparator)
     },
     getRegisterNumbers (organization) {
+      if (!organization.regulations) return null
+
       return organization.regulations
         .filter(regulation => regulation.type !== messageType)
         .map(regulation => regulation.registerNumber)
         .join(listTextSeparator)
+    },
+    getBusinessTypes(regulationTypeText) {
+      if (!regulationTypeText) return []
+
+      let regulationType = this.regulationTypes.find(item => item.regulationType === regulationTypeText)
+
+      return regulationType.businessTypes.map(item => item.type)
+    },
+    hasBusinessesWithInvasiveAnesthesia(regulationTypeText, businessTypeText) {
+      if (!regulationTypeText || !businessTypeText) return false
+
+      let regulationType = this.regulationTypes.find(item => item.regulationType === regulationTypeText)
+
+      let businessType = regulationType.businessTypes.find(item => item.type === businessTypeText)
+
+      return !!businessType.businessesWithInvasiveAnesthesia
+    },
+    getBusinessesWithInvasiveAnesthesia(regulationTypeText, businessTypeText) {
+      if (!regulationTypeText || !businessTypeText) return []
+
+      let regulationType = this.regulationTypes.find(item => item.regulationType === regulationTypeText)
+
+      let businessType = regulationType.businessTypes.find(item => item.type === businessTypeText)
+
+      return businessType.businessesWithInvasiveAnesthesia || []
+    },
+    addressChanged(location) {
+      this.searchParams.region = location.locationName
+      this.searchParams.district = location.locationUnitName
+      this.searchParams.settlement = location.settlement
     }
   },
-  computed: {}
+  computed: { },
+  components: {
+    'locations': locationsCOmponent
+  }
 }
 </script>
 
 <style scoped>
 .addButton {
   margin-left: 2vw;
+}
+
+.searchCheckbox {
+  margin-left: 15px;
+}
+
+.searchArea {
+  width: 50%;
+  margin: auto;
 }
 </style>
