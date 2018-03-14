b47bbcbbf4448f1f18d778c2783d34adcdeec055
irakli jiishkariani <irakli2692@gmail.com>
2018-03-14 17:20:34 +0400

    force soap 1.2

--- api/infrastructure/napr.service.js
+++ api/infrastructure/napr.service.js
@@ -3,7 +3,7 @@ const config = require('config')
 const RecordError = require('../exceptions/record.error')
 
 async function callNaprByTaxCode(taxCode) {
-  let client = await soap.createClientAsync(config.get('napr.soapAddress'))
+  let client = await soap.createClientAsync(config.get('napr.soapAddress'), {forceSoap12Headers: true})
 
   const args = {
     ObjectId: taxCode
